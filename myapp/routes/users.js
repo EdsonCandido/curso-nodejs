481a0ade5c9b494becd15692e731bb27620b63c9
Edson Cândido <edson.c.a.j@hotmail.com>
2019-08-25 00:41:46 -0300

    .

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/new', (req, res)=>{
+  res.render('newUser');
+});
+
+router.post('/', async (req, res)=>{
+  const user = await User.create(req.body);
+  res.redirect('login');
 });
 
 module.exports = router;
